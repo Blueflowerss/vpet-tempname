8c7252aa2148ec94a47a80e541f108b6b56106b6
Blueflowers <Canniverrumble@protonmail.com>
2025-03-25 14:47:07 +0100

    add camera panning

--- terrarium_camera.gd
+++ terrarium_camera.gd
@@ -1,1 +1,5 @@
 extends Camera2D
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+			position += -1*event.relative/zoom;
--- terrarium_scene.gd
+++ terrarium_scene.gd
@@ -4,6 +4,3 @@ func _ready() -> void:
 	astar_grid.region = Rect2i(0,0,64,64);
 	astar_grid.cell_size = Vector2(16,16);
 	astar_grid.update();
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouse:
-		print(event)
