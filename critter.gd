08629d21b7429dd5d09b0be9947e7476bb14bc86
Blueflowers <Canniverrumble@protonmail.com>
2025-03-28 10:08:22 +0100

    made the critter animation constant instead of linearly interpolated

--- critter.gd
+++ critter.gd
@@ -37,8 +37,10 @@ func _ready() -> void:
 	current_action = ACT.IDLE;
 func _physics_process(delta: float) -> void:
 	#decided to do animation here
-	position = lerp(position,Vector2((grid_position * 64) + Vector2i(32,32)),delta*3);
-	pass
+	#position = lerp(position,Vector2((grid_position * 64) + Vector2i(32,32)),delta*3);
+	var next_position : Vector2 = Vector2((grid_position * 64) + Vector2i(32,32));
+	var distance_to_next_tile : float = next_position.distance_to(position);
+	position += position.direction_to(next_position) * 10;
 func _process(delta: float) -> void:
 	#a kinda timer so the AI doesn't freak out
 	if act_timer < act_timer_length:
